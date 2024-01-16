5beb4f0b150b31a2769f7cd1d6d3f664ccc9d5f9
Arjit31 <129994490+Arjit31@users.noreply.github.com>
2024-01-16 23:41:08 +0530

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,12 +13,12 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("/auth/register", {
+      const res = await axios.post("https://backblog-qx9z.onrender.com/api/auth/register", {
         username,
         email,
         password
       });
-      res.data && window.location.replace("/login");
+      res.data && window.location.replace("https://backblog-qx9z.onrender.com/api/login");
     } catch (error) {
       setError(true);
       // console.log(error);
