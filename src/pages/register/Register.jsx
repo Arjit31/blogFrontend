da2d2e34e55da26c384a3c31c378c640c5701ab1
Arjit31 <129994490+Arjit31@users.noreply.github.com>
2024-01-17 00:37:25 +0530

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -18,7 +18,7 @@ export default function Register() {
         email,
         password
       });
-      res.data && window.location.replace("/login");
+      res.data && window.location.replace("/");
     } catch (error) {
       setError(true);
       // console.log(error);
